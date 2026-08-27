a74f63cf3c0435bc8c8fe6615bed434a30f2ad5e
wlsdn5968 <wlsdn5968@gmail.com>
2026-08-27 14:11:00 +0900

    deploy: yebu@581e3aa

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* 예부 서비스워커 — 네트워크 우선, 실패 시 캐시(오프라인 폴백).
-   캐시 키는 빌드마다 바뀌어(3294028782) 구버전이 눌러앉지 않는다. */
-var CACHE = 'yebu-3294028782';
+   캐시 키는 빌드마다 바뀌어(30e9ce9c81) 구버전이 눌러앉지 않는다. */
+var CACHE = 'yebu-30e9ce9c81';
 var ASSETS = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', function (e) {
